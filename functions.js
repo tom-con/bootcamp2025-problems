900ca634afbebe9ddfa6375a776a86c9b373db01
tom-con <tconchie@gmail.com>
2025-06-30 12:22:24 +0600

    Adds function skeletons

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
+
+function replace(str, charToReplace, strReplacement) {}
+
+function strLengthEven(str) {}
+
+function findRabbit(arr) {}
